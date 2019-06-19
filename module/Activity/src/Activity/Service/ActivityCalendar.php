533c981beec3a0bea257f83489cdaadc1e674e1e
Koen <koen.teuwen@gmail.com>
2019-06-19 16:06:21 +0200

    Fix

--- module/Activity/src/Activity/Service/ActivityCalendar.php
+++ module/Activity/src/Activity/Service/ActivityCalendar.php
@@ -126,7 +126,7 @@ class ActivityCalendar extends AbstractAclService
         $em->persist($proposal);
         $em->flush();
 
-        $options = $data['options'];
+        $options = $form->get('options');
         foreach ($options as $option) {
             $result = $this->createOption($option, $proposal->getId());
             if ($result == false) {
