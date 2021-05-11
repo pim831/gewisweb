4d68d3c99ba3ea5714d27a9e4990061d1232bac9
evert <e.j.c.v.d.linden@student.tue.nl>
2021-05-11 14:14:13 +0200

    test5

--- module/Activity/src/Activity/Controller/HelloWorldTest.php
+++ module/Activity/src/Activity/Controller/HelloWorldTest.php
@@ -3,22 +3,7 @@ use PHPUnit\Framework\TestCase;
 
 final class EmailTest extends TestCase
 {
-    public function testCanBeCreatedFromValidEmailAddress()
-    {
-        $this->assertInstanceOf(
-            Email::class,
-            Email::fromString('user@example.com')
-        );
-    }
-
-    public function testCannotBeCreatedFromInvalidEmailAddress()
-    {
-        $this->expectException(InvalidArgumentException::class);
-
-        Email::fromString('invalid');
-    }
-
-    public function testCanBeUsedAsString()
+       public function testCanBeUsedAsString()
     {
         $this->assertEquals(
             'user@example.com',
@@ -26,6 +11,5 @@ final class EmailTest extends TestCase
         );
     }
 }
-
 
 
